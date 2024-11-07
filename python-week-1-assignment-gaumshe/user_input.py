a8ce4c7657a6e503ba91a1355e3e9e8ee3392d5b
Gall <onyenobigall@gmail.com>
2024-11-07 02:07:45 +0100

    assignment comitted for the user input function that collects basic data

--- python-week-1-assignment-gaumshe/user_input.py
+++ python-week-1-assignment-gaumshe/user_input.py
@@ -1,8 +1,10 @@
-# print("what is your name")
+# defining functions for name , age and location
+Name = input("Your Name: ")
+Age = input("Your Age: ")
+Location = input("Your Location: ")
 
-# defining a function using python
-def my_Function(): 
-    print("this is my function")
-
-my_Function()
+# the responses in the terminal to make it more interactive
+print( "Hi ", Name, "we noticed that you are ", Age, " years old",
+       " ensure that  you are up to 18 years to access the website",
+         " We also acknoledge your diversity","You are welcome from ", Location )
 
