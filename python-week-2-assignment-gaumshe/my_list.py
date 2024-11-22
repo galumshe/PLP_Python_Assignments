82afc5a0b8ca38188c3c790787699398854d1fc3
Gall <onyenobigall@gmail.com>
2024-11-22 02:59:06 +0100

    week 2 assignment operations with lists complete

--- python-week-2-assignment-gaumshe/my_list.py
+++ python-week-2-assignment-gaumshe/my_list.py
@@ -17,3 +17,15 @@ print(my_list)
 # Extend my_list with another list: .
 my_list.extend([50, 60, 70])
 print(my_list)
+
+# Remove the last element from my_list
+my_list.remove(70)
+print(my_list)
+
+# Sort my_list in ascending order.
+my_list.sort()
+print(my_list)
+
+# Find and print the index of the value 30 in my_list.
+index_30 = my_list.index(30)
+print(index_30)
