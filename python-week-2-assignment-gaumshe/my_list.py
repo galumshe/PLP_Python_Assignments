6fe1aedea6a3d22f935d460f688f5c1e3c1b1219
Gall <onyenobigall@gmail.com>
2024-11-22 02:55:45 +0100

    just completed the assignment and i got some good lessons on lists in python

---
+++ python-week-2-assignment-gaumshe/my_list.py
@@ -0,0 +1,19 @@
+# creating my_list 
+my_list = []
+print(my_list)
+
+# appening the following elements to my_list: 10, 20, 30, 40.
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+# inserting the value 15 at the second position in the list
+my_list.insert(2, 15)
+print(my_list)
+
+# Extend my_list with another list: .
+my_list.extend([50, 60, 70])
+print(my_list)
